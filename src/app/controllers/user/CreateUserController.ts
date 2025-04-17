76fac476235ef1929758c06e018ff692fe6e7fc9
Alvaro <alvarorecabal.v@gmail.com>
2025-04-17 10:56:21 -0400

    fix: normalize line endings

---
+++ src/app/controllers/user/CreateUserController.ts
@@ -0,0 +1,7 @@
+import { BaseController } from '../BaseController';
+
+export class CreateUserController implements BaseController {
+  run(): void {
+    throw new Error('Method not implemented.');
+  }
+}
